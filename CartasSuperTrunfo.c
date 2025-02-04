2d3c2a1b84939308dfd00eee1e64786628653ab9
genildo354 <genildo354@gmail.com>
2025-02-04 10:52:34 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,23 +2,94 @@
 
 int main(){
 
-printf(" DESAFIO CARTAS \n");
 
-char estado[10] = "BAHIA";
+
+
+
+printf("                                                 DESAFIO CARTAS \n");
+
+printf(" \n --------------------------------");
+
+printf(" \n(PRIMEIRA CARTA) \n");
+
+char estado[10] ="BAHIA";
 char carta[10] = "A01";
-char cidade[10] = "SALVADOR";
+char cidade[100] = "(SALVADOR)";
 int populacao = 2418;
 float pib = 62000;
-float area = 693;
+float area = 693000;
 int pontos = 78;
 
-printf("ESTADO: %s\n", estado);
-printf("CODIGO DA CARTA: %s\n", carta);
-printf("CIDADE: %s\n", cidade);    
-printf("POPULAÇÃO: %d \n", populacao);
+printf("ESTADO: %s\n",estado);
+printf("CODIGO DA CARTA: %s \n", carta);
+printf("CIDADE: %s \n", cidade);    
+printf("POPULAÇÃO: %d \n" , populacao);
 printf("ÁREA EM Km²: %f \n", area);
 printf("PIB: %.2f \n", pib);
 printf("PONTOS TURISTICOS: %.d \n", pontos);
+
+printf(" \n --------------------------------");
+
+printf(" \n(SEGUNDA CARTA) \n");
+
+char estado2[20] = "BAHIA";
+char carta2[20] = "A02";
+char cidade2[200] ="(JUAZEIRO DA BAHIA)";
+int populacao2 =2378;
+float pib2 =26600;
+float area2 =721200;
+int pontos2 =28;
+
+
+printf("ESTADO: %s\n",estado2);
+printf("CODIGO DA CARTA: %s \n", carta2);
+printf("CIDADE: %s \n", cidade2);    
+printf("POPULAÇÃO: %d \n" , populacao2);
+printf("ÁREA EM Km²: %f \n", area2);
+printf("PIB: %.2f \n", pib2);
+printf("PONTOS TURISTICOS: %.d \n", pontos2);
+
+printf(" \n --------------------------------");
+
+printf(" \n(TERCEIRA CARTA) \n");
+
+char estado3[20] = "BAHIA";
+char carta3[20] = "A03";
+char cidade3[200] ="(FEIRA DE SANTANA)";
+int populacao3 =616000;
+float pib3 =27691;
+float area3 =1304;
+int pontos3=30;
+
+
+printf("ESTADO: %s\n",estado3);
+printf("CODIGO DA CARTA: %s \n", carta3);
+printf("CIDADE: %s \n", cidade3);    
+printf("POPULAÇÃO: %d \n", populacao3);
+printf("ÁREA EM Km²: %f \n", area3);
+printf("PIB: %.2f \n", pib3);
+printf("PONTOS TURISTICOS: %.d \n", pontos3);
+
+printf(" \n --------------------------------");
+
+printf(" \n(QUARTA CARTA) \n");
+
+char estado4[20] = "BAHIA";
+char carta4[20] = "A04";
+char cidade4[200] ="(ANTAS-BA)";
+int populacao4 =14206;
+float pib4=8179;
+float area4 =383900;
+int pontos4=10;
+
+
+printf("ESTADO: %s\n",estado4);
+printf("CODIGO DA CARTA: %s \n", carta4);
+printf("CIDADE: %s \n", cidade4);    
+printf("POPULAÇÃO: %d \n", populacao4);
+printf("ÁREA EM Km²: %f \n", area4);
+printf("PIB: %.2f \n", pib4);
+printf("PONTOS TURISTICOS: %.d \n", pontos4);
 
 
 
