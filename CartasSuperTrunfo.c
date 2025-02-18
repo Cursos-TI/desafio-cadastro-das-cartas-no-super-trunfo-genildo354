b268ba0745daa0be792a4d2ac520767cc7b40089
genildo354 <genildo354@gmail.com>
2025-02-18 17:31:08 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,14 +2,21 @@
 
 int main(){
 
-    char estado , carta1, carta2, carta3, carta4;
-    char cidade1, cidade2, cidade3, cidade4; 
-    int populacao1, populacao2, populacao3, populacao4;
-    int ponto1, ponto2,ponto3,ponto4;
-    float pib1, pib2, pib3, pib4;
-    float area1, area2, area3, area4;
-
-
+    char estado1[10] = "A";
+    char estado2[10] = "B";
+    char carta1[10] = "A01";
+    char carta2[10] = "B02";
+    char cidade1[50] ="SÃO PAULO";
+    char cidade2[50]="RIO DE JANEIRO";
+    int populacao1= 11451999;
+    int populacao2=6729894;
+    int ponto1=50;
+    int ponto2=30;
+    float pib1=699.28;
+    float pib2= 300.50;
+    float area1= 1521;
+    float area2 = 1200;
+    
 
 printf("                                                 DESAFIO CARTAS (SUPER TRUNFO)\n");
 
@@ -19,96 +26,23 @@ printf("\n(PRIMEIRA CARTA) \n");
 
 
 
-printf("ESTADO: ");
-scanf("%s", &estado);
+printf("ESTADO: %s\n", estado1);
+printf("CARTA: %s\n",carta1);
+printf("CIDADE: %s\n", cidade1);
+printf("POPULAÇÃO: %d\n", populacao1);
+printf("AREAM EM KM²: %3.f\n", area1);
+printf("PIB: %.2f\n", pib1);
+printf("NÚMERO DE PONTOS TURÍSTICOS: %d\n", ponto1);
 
-printf("CARTA: ");
-scanf("%s", &carta1);
-
-printf("CIDADE: ");
-scanf("%s", &cidade1);
-
-printf("POPULAÇÃO: ");
-scanf("%d", &populacao1);
-
-printf("PIB: ");
-scanf("%d", &pib1);
-
-printf("AREAM EM KM²: ");
-scanf("%d", &area1);
-
-printf("PONTOS TURÍSTICOS: ");
-scanf("%d", &ponto1);
 
 printf("\n(SEGUNDA CARTA) \n");
 
-printf("ESTADO: ");
-scanf("%s", &estado);
-
-printf("CARTA: ");
-scanf("%s", &carta2);
-
-printf("CIDADE: ");
-scanf("%s", &cidade2);
-
-printf("POPULAÇÃO: ");
-scanf("%d", &populacao2);
-
-printf("PIB: ");
-scanf("%d", &pib2);
-
-printf("AREAM EM KM²: ");
-scanf("%d", &area2);
-
-printf("PONTOS TURÍSTICOS: ");
-scanf("%d", &ponto2);
-
-printf("\n(TERCEIRA CARTA) \n");
-
-printf("ESTADO: ");
-scanf("%s", &estado);
-
-printf("CARTA: ");
-scanf("%s", &carta3);
-
-printf("CIDADE: ");
-scanf("%s", &cidade3);
-
-printf("POPULAÇÃO: ");
-scanf("%d", &populacao3);
-
-printf("PIB: ");
-scanf("%d", &pib3);
-
-printf("AREAM EM KM²: ");
-scanf("%d", &area3);
-
-printf("PONTOS TURÍSTICOS: ");
-scanf("%d", &ponto3);
-
-printf("\n(QUARTA CARTA) \n");
-
-printf("ESTADO: ");
-scanf("%s", &estado);
-
-printf("CARTA: ");
-scanf("%s", &carta4);
-
-printf("CIDADE: ");
-scanf("%s", &cidade4);
-
-printf("POPULAÇÃO: ");
-scanf("%d", &populacao4);
-
-printf("PIB: ");
-scanf("%d", &pib4);
-
-printf("AREAM EM KM²: ");
-scanf("%d", &area4);
-
-printf("PONTOS TURÍSTICOS: ");
-scanf("%d", &ponto4);
-
-return 0;
+printf("ESTADO: %s\n", estado2);
+printf("CARTA: %s\n",carta2);
+printf("CIDADE: %s\n", cidade2);
+printf("POPULAÇÃO: %d\n", populacao2);
+printf("AREAM EM KM²: %3.f\n", area2);
+printf("PIB: %.2f\n", pib2);
+printf("NÚMERO DE PONTOS TURÍSTICOS: %d\n", ponto2);
 
 }
