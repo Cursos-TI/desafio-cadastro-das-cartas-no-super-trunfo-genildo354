f8d1da766882d3aec671489f9bf358114b7a25fe
genildo354 <genildo354@gmail.com>
2025-02-22 13:46:54 +0000

    NOVO CO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,8 +17,6 @@ printf("\n --------------------------------");
 
 printf("\n(PRIMEIRA CARTA) \n");
 
-
-
 printf("ESTADO: ");
 scanf("%s", &estado);
 
@@ -34,7 +32,7 @@ scanf("%d", &populacao1);
 printf("PIB: ");
 scanf("%d", &pib1);
 
-printf("AREAM EM KM²: ");
+printf("AREA EM KM²: ");
 scanf("%d", &area1);
 
 printf("PONTOS TURÍSTICOS: ");
@@ -57,57 +55,12 @@ scanf("%d", &populacao2);
 printf("PIB: ");
 scanf("%d", &pib2);
 
-printf("AREAM EM KM²: ");
+printf("AREA EM KM²: ");
 scanf("%d", &area2);
 
 printf("PONTOS TURÍSTICOS: ");
 scanf("%d", &ponto2);
 
-printf("\n(TERCEIRA CARTA) \n");
-
-printf("ESTADO: ");
-scanf("%s", &estado);
-
-printf("CARTA: ");
-scanf("%s", &carta3);
-
-printf("CIDADE: ");
-scanf("%s", &cidade3);
-
-printf("POPULAÇÃO: ");
-scanf("%d", &populacao3);
-
-printf("PIB: ");
-scanf("%d", &pib3);
-
-printf("AREAM EM KM²: ");
-scanf("%d", &area3);
-
-printf("PONTOS TURÍSTICOS: ");
-scanf("%d", &ponto3);
-
-printf("\n(QUARTA CARTA) \n");
-
-printf("ESTADO: ");
-scanf("%s", &estado);
-
-printf("CARTA: ");
-scanf("%s", &carta4);
-
-printf("CIDADE: ");
-scanf("%s", &cidade4);
-
-printf("POPULAÇÃO: ");
-scanf("%d", &populacao4);
-
-printf("PIB: ");
-scanf("%d", &pib4);
-
-printf("AREAM EM KM²: ");
-scanf("%d", &area4);
-
-printf("PONTOS TURÍSTICOS: ");
-scanf("%d", &ponto4);
 
 return 0;
 
